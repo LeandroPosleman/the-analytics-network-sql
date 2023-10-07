ebdf1f0822490483aafbd07a4fe116a826b2444b
LeandroPosleman <89052804+LeandroPosleman@users.noreply.github.com>
2023-10-07 12:23:59 -0300

    Update project-answers.sql

--- parte-1/project/project-answers.sql
+++ parte-1/project/project-answers.sql
@@ -1,6 +1,21 @@
 
 -- General 
 -- - Ventas brutas, netas y margen (USD)
+with cte1 as
+		(select order_number,
+		CASE 
+			WHEN currency = 'ARS' THEN sale/fx_rate_usd_peso
+			WHEN currency = 'URU' THEN sale/fx_rate_usd_uru
+			WHEN currency = 'EUR' THEN sale/fx_rate_usd_eur
+			ELSE sale
+			END AS venta_en_usd
+		from stg.order_line_sale ols
+		left join stg.monthly_average_fx_rate fx
+		on date_trunc('month',ols.date) = fx.month)
+select sum(venta_en_usd) as venta_bruta_usd
+from cte1
+
+
 
 -- - Margen por categoria de producto (USD)
 
