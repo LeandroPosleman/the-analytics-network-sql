afead485d53bc795f37b887d68e021dc12e7200b
LeandroPosleman <89052804+LeandroPosleman@users.noreply.github.com>
2023-10-07 12:59:42 -0300

    Update project-answers.sql

--- parte-1/project/project-answers.sql
+++ parte-1/project/project-answers.sql
@@ -1,6 +1,7 @@
 
 -- General 
 -- - Ventas brutas, netas y margen (USD)
+-- bruta --
 with cte1 as
 		(select order_number,
 		CASE 
@@ -14,8 +15,29 @@ with cte1 as
 		on date_trunc('month',ols.date) = fx.month)
 select sum(venta_en_usd) as venta_bruta_usd
 from cte1
+ -- neta --
+with cte1 as 
 
-
+	(select order_number,
+		CASE 
+			WHEN currency = 'ARS' THEN sale/fx_rate_usd_peso
+			WHEN currency = 'URU' THEN sale/fx_rate_usd_uru
+			WHEN currency = 'EUR' THEN sale/fx_rate_usd_eur
+			ELSE sale
+			END AS venta_en_usd,
+		CASE
+			WHEN currency = 'ARS' THEN promotion/fx_rate_usd_peso
+			WHEN currency = 'URU' THEN promotion/fx_rate_usd_uru
+			WHEN currency = 'EUR' THEN promotion/fx_rate_usd_eur
+			ELSE promotion
+			END AS promotion_en_usd
+		from stg.order_line_sale ols
+		left join stg.monthly_average_fx_rate fx
+		on date_trunc('month',ols.date) = fx.month)
+		
+select *,venta_en_usd - coalesce(promotion_en_usd,0) as ventas_netas
+from cte1
+-- margen --
 
 -- - Margen por categoria de producto (USD)
 
