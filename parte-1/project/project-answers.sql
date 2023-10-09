fdc465fb95d6032f1d4067d477067f38d997a907
LeandroPosleman <89052804+LeandroPosleman@users.noreply.github.com>
2023-10-09 15:25:51 -0300

    Update project-answers.sql

--- parte-1/project/project-answers.sql
+++ parte-1/project/project-answers.sql
@@ -63,7 +63,33 @@ with cte1 as
 select *, (venta_en_usd - coalesce(promotion_en_usd,0) - costo_linea) as margin
 from cte1
 -- - Margen por categoria de producto (USD)
+with cte1 as
 
+(select pm.category,
+		CASE 
+			WHEN currency = 'ARS' THEN sale/fx_rate_usd_peso
+			WHEN currency = 'URU' THEN sale/fx_rate_usd_uru
+			WHEN currency = 'EUR' THEN sale/fx_rate_usd_eur
+			ELSE sale
+			END AS venta_en_usd,
+		CASE
+			WHEN currency = 'ARS' THEN promotion/fx_rate_usd_peso
+			WHEN currency = 'URU' THEN promotion/fx_rate_usd_uru
+			WHEN currency = 'EUR' THEN promotion/fx_rate_usd_eur
+			ELSE promotion
+			END AS promotion_en_usd,
+		(c.product_cost_usd*ols.quantity) as costo_linea
+		from stg.order_line_sale ols
+		left join stg.monthly_average_fx_rate fx
+		on date_trunc('month',ols.date) = fx.month
+		left join stg.cost c
+		on c.product_code = ols.product
+		left join stg.product_master pm
+		on pm.product_code = ols.product)
+
+select category, sum(venta_en_usd - coalesce(promotion_en_usd,0) - costo_linea) as margin
+from cte1
+group by category
 -- - ROI por categoria de producto. ROI = ventas netas / Valor promedio de inventario (USD)
 
 -- - AOV (Average order value), valor promedio de la orden. (USD)
